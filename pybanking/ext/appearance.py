ddbc2deeab9d4286f87f4339ec787f8dca43a6ca
Eduardo Lira <eduardolirainfo@gmail.com>
2022-12-09 01:24:28 -0300

    create HTML templates and  structured clean definitions

--- pybanking/ext/appearance.py
+++ pybanking/ext/appearance.py
@@ -2,7 +2,7 @@ from flask_assets import Bundle, Environment
 
 
 def init_app(app):
-
+    # thinking about using this for the css
     assets = Environment(app)
 
     css = Bundle('src/main.css', output='dist/main.css',
